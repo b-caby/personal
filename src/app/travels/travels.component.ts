e7092e272c015e109b28b148a20d628ca1d83a89
Berenger Caby <caby.berenger@gmail.com>
2024-04-15 18:09:19 +0200

    Feat: starting to put data on the map

--- src/app/travels/travels.component.ts
+++ src/app/travels/travels.component.ts
@@ -1,21 +1,21 @@
 import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { AddLayerObject, GeoJSONSourceSpecification, Map } from 'maplibre-gl';
 import { TravelService } from './service/travels.service';
 import { Trip } from './model/trip';
-import moment from 'moment';
 import { StepComponent } from "./step/step.component";
 import { TripComponent } from "./trip/trip.component";
+import { Step } from './model/step';
 
 @Component({
-    selector: 'app-travels',
-    standalone: true,
-    templateUrl: './travels.component.html',
-    styleUrl: './travels.component.css',
-    imports: [StepComponent, TripComponent]
+  selector: 'app-travels',
+  standalone: true,
+  templateUrl: './travels.component.html',
+  styleUrl: './travels.component.css',
+  imports: [StepComponent, TripComponent]
 })
 export class TravelsComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  private map: Map | undefined;
+  private map!: Map;
   public currentTrip: Trip | undefined;
   public trips: Trip[] = [];
 
@@ -37,6 +37,75 @@ export class TravelsComponent implements OnInit, AfterViewInit, OnDestroy {
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom
     });
+
+    this.map.on("load", () => {
+      this.trips.forEach(trip => {
+        this.map.addSource(trip.title, this.createSource(trip.steps));
+        this.map.addLayer(this.createMarkerLayer(trip.title));
+        this.map.addLayer(this.createLineLayer(trip.title));
+      });
+    });
+  }
+
+  private createSource(steps: Step[]): GeoJSONSourceSpecification {
+    const features: any[] = [];
+    const coordinates: any[] = [];
+
+    /* Create the point feature */
+    steps.forEach(step => {
+      const latLong = [step.longitude, step.latitude]
+
+      features.push({
+        type: "Feature",
+        geometry: { type: "Point", coordinates: latLong },
+        properties: {}
+      });
+
+      coordinates.push(latLong);
+    });
+
+    /* Create the line feature */
+    features.push({
+      type: "Feature",
+      geometry: { type: "LineString", coordinates: coordinates },
+    });  
+
+    /* Create the feature collection */
+    return {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: features
+      }
+    };
+  }
+
+  private createMarkerLayer(source: string): AddLayerObject {
+    return {
+      id: `marker-${source}`,
+      type: "circle",
+      source: source,
+      paint: {
+        "circle-color": "red",
+        "circle-radius": 5
+      }
+    };
+  }
+
+  private createLineLayer(source: string): AddLayerObject {
+    return {
+      id: `line-${source}`,
+      type: "line",
+      source: source,
+      layout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      paint: {
+        "line-color": 'red',
+        "line-width": 2
+    }
+    };
   }
 
   ngOnDestroy() {
@@ -47,7 +116,7 @@ export class TravelsComponent implements OnInit, AfterViewInit, OnDestroy {
     this.currentTrip = trip;
   }
 
-  public CloseTrip(){
+  public CloseTrip() {
     this.currentTrip = undefined;
   }
 }
